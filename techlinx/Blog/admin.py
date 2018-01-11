af6c1569b2969300ff3e0edd7f7db58335897490
LigorioSalgado <salgadolg5@hotmail.com>
2018-01-10 20:17:23 -0600

    Agregando modelos al admin

--- techlinx/Blog/admin.py
+++ techlinx/Blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Post,Categories
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CategorieAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Categories,CategorieAdmin)
