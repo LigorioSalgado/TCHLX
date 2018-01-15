a5d886b710c3aec9a19fc495c920cdc771937ab2
LigorioSalgado <salgadolg5@hotmail.com>
2018-01-14 22:51:30 -0600

    Agregando  textfield con esteroides

--- techlinx/Blog/admin.py
+++ techlinx/Blog/admin.py
@@ -2,15 +2,21 @@ from django.contrib import admin
 from .models import Post, Categories
 from techlinx.Staff.models import Staff
 # Register your models here.
+from django_summernote.admin import SummernoteModelAdmin
 
 
-class PostAdmin(admin.ModelAdmin):
-    fields = ('titulo','contenido','cover','categoria','publicado','time_estimate')
+class PostAdmin(SummernoteModelAdmin):
+    summer_note_fields = ('contenido',)
+    fields = ('titulo','contenido','cover','categoria','publicado')
+    readonly_fields = ('time_estimate',)
    
     def get_queryset(self, request):
         qs = super(PostAdmin, self).get_queryset(request)
-        print(request.user.usuario.editor)
-        if request.user.is_superuser or request.user.usuario.editor:
+        try:
+            editor = Staff.objects.filter(user=request.user,editor=True).exist()
+        except:
+            editor = False
+        if request.user.is_superuser or editor:
             return qs
         return qs.filter(autor__user=request.user)
 
@@ -22,10 +28,12 @@ class PostAdmin(admin.ModelAdmin):
         return instance.autor
 
     def time_estimate(self, instance):
-        return instance.tiempo_estimado
+        return instance.tiempo_estimados
+
     author.short_description = "Autor del post"
     time_estimate.short_description = "Tiempo estimado de lectura"
 
+    
 
 class CategorieAdmin(admin.ModelAdmin):
     pass
