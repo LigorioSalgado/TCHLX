44160c9ba5bcb3e2ed18d8716006f810b432a2a0
Ligorio <salgadolg5@hotmail.com>
2018-01-23 13:41:04 -0600

    paginacion completa

--- techlinx/Blog/views.py
+++ techlinx/Blog/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post,Categories
 from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 
 
 def index(request):
     imagen = "/static/img/sidebar.jpg"
-    posts =  Post.objects.filter(publicado=True).select_related('autor').order_by('-fecha_publicacion')
+    posts_list =  Post.objects.filter(publicado=True).select_related('autor').order_by('-fecha_publicacion')
     categories = Categories.objects.all()
-    print(posts.count())
+    paginator = Paginator(posts_list, 5)
+    page = request.GET.get('page',1)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+
+
 
     return render(request,'blog/index.html',{'image':imagen,'posts':posts, 'categories':categories})
 
